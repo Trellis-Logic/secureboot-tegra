4974b8b881cdb19da7e6a1979d99ce2ff5d46840
Tim Orling <tim.orling@konsulko.com>
2022-03-18 11:25:06 -0700

    download-and-prepare-files: use BASEURL
    Use the BASEURL variable from downloadfilenames to simplify this script
    and localize changes in one place.
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- download-and-prepare-files.sh
+++ download-and-prepare-files.sh
@@ -5,13 +5,13 @@ source ./downloadfilenames
 mkdir -p ${DOWNLOADS_DIR}
 pushd ${DOWNLOADS_DIR}
 if [ ! -f ${L4T_RELEASE_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${L4T_RELEASE_PACKAGE_NAME}
+    wget ${BASEURL}/${L4T_RELEASE_PACKAGE_NAME}
 fi
 if [ ! -f ${SECUREBOOT_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${SECUREBOOT_PACKAGE_NAME}
+    wget ${BASEURL}/${SECUREBOOT_PACKAGE_NAME}
 fi
 if [ ! -f ${SAMPLE_FS_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${SAMPLE_FS_PACKAGE_NAME}
+    wget ${BASEURL}/${SAMPLE_FS_PACKAGE_NAME}
 fi
 popd
 ./preparing-for-use.sh

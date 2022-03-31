5e7c9ab86a9c8bb248b2c7dd48e2937f3045c92e
Tim Orling <tim.orling@konsulko.com>
2022-03-30 20:19:59 -0700

    preparing-for-use.sh: patch for python3
    tegrasign_v3.py erroneously has a #!/usr/bin/env python
    when it should be #!/usr/bin/env python3.
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- preparing-for-use.sh
+++ preparing-for-use.sh
@@ -5,6 +5,8 @@ cd `dirname $0`
 source ./downloadfilenames
 echo "Extracting ${L4T_RELEASE_PACKAGE}"
 sudo tar xpf ${L4T_RELEASE_PACKAGE}
+# See https://forums.developer.nvidia.com/t/postmortem-jetson-xavier-agx-will-not-get-past-boot-rom-after-burning-pkc-sbk-kek256/208426/5?u=ticotimo
+patch -p0 < 0001-tegrasign-v3-python3.patch
 cd Linux_for_Tegra/rootfs/
 echo "Extracting ${SAMPLE_FS_PACKAGE}"
 sudo tar xpf ${SAMPLE_FS_PACKAGE}

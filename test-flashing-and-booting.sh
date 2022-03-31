225a7142c6825760ac11ae2e5cdaf61a7398eb10
Tim Orling <tim.orling@konsulko.com>
2022-03-30 17:37:59 -0700

    flashing-and-booting.sh: jetson-agx-xavier-devkit
    Add test-flashing-and-booting.sh with the --no-flash option
    
    NOTE: this actually DOES flash the OS to the device!
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- test-flashing-and-booting.sh
+++ test-flashing-and-booting.sh
@@ -3,4 +3,4 @@
 # https://docs.nvidia.com/jetson/l4t/index.html#page/Tegra%2520Linux%2520Driver%2520Package%2520Development%2520Guide%2Fflashing.html%23
 cd `dirname $0`
 cd Linux_for_Tegra
-sudo ./flash.sh jetson-agx-xavier-devkit mmcblk0p1
+sudo ./flash.sh –-no-flash --sign -u rsa_pub.pem -v sbk_hex_file jetson-agx-xavier-devkit mmcblk0p1
